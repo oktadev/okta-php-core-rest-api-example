6bed2150a67e872d662a85a1e22a21bba4d5b783
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-12-02 02:26:14 +0200

    Added endpoint POST /person

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -65,4 +65,36 @@ class PersonController {
         $response['body'] = json_encode($result);
         return $response;
     }
+
+    private function createUserFromRequest()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (! $this->validatePerson($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->personGateway->insert($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function validatePerson($input)
+    {
+        if (! isset($input['firstname'])) {
+            return false;
+        }
+        if (! isset($input['lastname'])) {
+            return false;
+        }
+        return true;
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
+    }
 }
