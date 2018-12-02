2b2c6d700621197b96095f3e8884b31fc0dd38b0
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-12-02 13:32:51 +0200

    Added Okta authentication to the API

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
+$uri = explode('/', $uri);
 
 // all of our endpoints start with /person
 // everything else results in a 404 Not Found
@@ -18,10 +18,16 @@ if ($uri[1] !== 'person') {
     exit();
 }
 
-// the user id is of course optional and must be a number:
+// the user id is, of course, optional and must be a number:
 $userId = null;
 if (isset($uri[2])) {
     $userId = (int) $uri[2];
+}
+
+// authenticate the request with Okta:
+if (! authenticate()) {
+    header("HTTP/1.1 401 Unauthorized");
+    exit('Unauthorized');
 }
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
@@ -29,5 +35,33 @@ $requestMethod = $_SERVER["REQUEST_METHOD"];
 // pass the request method and user ID to the PersonController:
 $controller = new PersonController($dbConnection, $requestMethod, $userId);
 $controller->processRequest();
+
+function authenticate() {
+    try {
+        switch(true) {
+            case array_key_exists('HTTP_AUTHORIZATION', $_SERVER) :
+                $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+                break;
+            case array_key_exists('Authorization', $_SERVER) :
+                $authHeader = $_SERVER['Authorization'];
+                break;
+            default :
+                $authHeader = null;
+                break;
+        }
+        preg_match('/Bearer\s(\S+)/', $authHeader, $matches);
+        if(!isset($matches[1])) {
+            throw new \Exception('No Bearer Token');
+        }
+        $jwtVerifier = (new \Okta\JwtVerifier\JwtVerifierBuilder())
+            ->setIssuer(getenv('OKTAISSUER'))
+            ->setAudience('api://default')
+            ->setClientId(getenv('OKTACLIENTID'))
+            ->build();
+        return $jwtVerifier->verify($matches[1]);
+    } catch (\Exception $e) {
+        return false;
+    }
+}
 
 ?>
