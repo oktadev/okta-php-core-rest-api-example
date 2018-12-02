c6505fb6009c7411a4e15e2ebd8481c6a5af8390
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-12-02 03:06:22 +0200

    Implemented PUT /person/{id} and DELETE /person/{id}, added validation for existing user, some refactoring of the responses

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -40,8 +40,7 @@ class PersonController {
                 $response = $this->deleteUser($this->userId);
                 break;
             default:
-                $response['body'] = null;
-                $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+                $response = $this->notFoundResponse();
                 break;
         }
         header($response['status_code_header']);
@@ -61,6 +60,9 @@ class PersonController {
     private function getUser($id)
     {
         $result = $this->personGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
         $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['body'] = json_encode($result);
         return $response;
@@ -74,6 +76,34 @@ class PersonController {
         }
         $this->personGateway->insert($input);
         $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function updateUserFromRequest($id)
+    {
+        $result = $this->personGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (! $this->validatePerson($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->personGateway->update($id, $input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function deleteUser($id)
+    {
+        $result = $this->personGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $this->personGateway->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['body'] = null;
         return $response;
     }
@@ -95,6 +125,13 @@ class PersonController {
         $response['body'] = json_encode([
             'error' => 'Invalid input'
         ]);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
         return $response;
     }
 }
